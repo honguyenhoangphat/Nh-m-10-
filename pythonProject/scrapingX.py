a9fe23c3e0a5fcb62af4c9e18f6d3644cf1cf469
Levan-24714 <vtran0712004@gmail.com>
2024-10-19 10:32:43 +0700

    update

--- pythonProject/scrapingX.py
+++ pythonProject/scrapingX.py
@@ -8,17 +8,25 @@ driver.get("https://x.com/i/flow/login")
 
 time.sleep(5)
 
-username = driver.find_element(By.XPATH, "//input[@name='text']")
-username.send_keys("hnhp113")
-name = driver.find_element(By.XPATH,"//span[contains(text(),'Next')]")
-name.click()
+# username = driver.find_element(By.XPATH, "//input[@name='text']")
+# username.send_keys("hnhp113")
+# name = driver.find_element(By.XPATH, "//span[contains(text(),'Next')]")
+# name.click()
 
 time.sleep(2)
 
+email = driver.find_element(By.XPATH, " //input[@name='text']")
+email.send_keys("hnhp113114115@gmail.com")
+mail =driver.find_element(By.XPATH, "//span[contains(text(),'Next')]")
+mail.click()
+
+time.sleep(5)
+
 password = driver.find_element(By.XPATH, "//input[@name='password']")
 password.send_keys("phatho0317")
-pw = driver.find_element(By.XPATH,"//span[contains(text(),'Log in')]")
+pw = driver.find_element(By.XPATH, "//span[contains(text(),'Log in')]")
 pw.click()
+
 
 time.sleep(5)
 search = driver.find_element(By.XPATH, "//input[@data-testid='SearchBox_Search_Input']")
@@ -27,11 +35,11 @@ search.send_keys(idol)
 search.send_keys(Keys.ENTER)
 
 time.sleep(2)
-people = driver.find_element(By.XPATH,"//span[contains(text(),'People')]")
+people = driver.find_element(By.XPATH, "//span[contains(text(),'People')]")
 people.click()
 
 time.sleep(2)
-name_idol = driver.find_element(By.XPATH,"//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div/div[3]/section/div/div/div[1]/div/div/button/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span/span[1]")
+name_idol = driver.find_element(By.XPATH, "//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div/div[3]/section/div/div/div[1]/div/div/button/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span/span[1]")
 name_idol.click()
 
 #
@@ -44,7 +52,7 @@ resport=[]
 view=[]
 tweetIMG=[]
 
-userID = driver.find_element(By.XPATH,"//div[data-testid='User-Name']").text
+userID = driver.find_element(By.XPATH, "//div[data-testid='User-Name']").text
 timePost = driver.find_element(By.XPATH,"//time").get_attribute("datetime")
 tweetText = driver.find_element(By.XPATH,"//div[data-testid='tweetText']")
 like = driver.find_element(By.XPATH,"//div[data-testid='like']")
