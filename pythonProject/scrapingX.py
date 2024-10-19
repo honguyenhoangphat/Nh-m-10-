a5a7ef8b680734bab392a4b1a7222495e34b7b94
Phat <phatho0317@gmail.com>
2024-10-19 20:13:11 +0700

    .

--- pythonProject/scrapingX.py
+++ pythonProject/scrapingX.py
@@ -11,51 +11,36 @@ driver.get("https://x.com/i/flow/login")
 
 time.sleep(5)
 
-# username = driver.find_element(By.XPATH, "//input[@name='text']")
-# username.send_keys("hnhp113")
-# name = driver.find_element(By.XPATH, "//span[contains(text(),'Next')]")
-# name.click()
+username = driver.find_element(By.XPATH, "//input[@name='text']")
+username.send_keys("hnhp113")
+name = driver.find_element(By.XPATH, "//span[contains(text(),'Next')]")
+name.click()
 
 time.sleep(2)
-
-email = driver.find_element(By.XPATH, " //input[@name='text']")
-email.send_keys("hnhp113114115@gmail.com")
-mail =driver.find_element(By.XPATH, "//span[contains(text(),'Next')]")
-mail.click()
-
-time.sleep(5)
 
 password = driver.find_element(By.XPATH, "//input[@name='password']")
 password.send_keys("phatho0317")
 pw = driver.find_element(By.XPATH, "//span[contains(text(),'Log in')]")
 pw.click()
 
-<<<<<<< HEAD
-time.sleep(8)
-=======
-
 time.sleep(5)
->>>>>>> 40f7eae8317acfc20488b2430bac7a698f4f2eec
 search = driver.find_element(By.XPATH, "//input[@data-testid='SearchBox_Search_Input']")
 idol = "M-TP"
 search.send_keys(idol)
 search.send_keys(Keys.ENTER)
 
-<<<<<<< HEAD
 time.sleep(5)
 people = driver.find_element(By.XPATH,"//span[contains(text(),'People')]")
 people.click()
 
 time.sleep(7)
 name_idol = driver.find_element(By.XPATH,"//*[@id='react-root']/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[1]/div/div/button/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span/span[1]")
-=======
 time.sleep(2)
 people = driver.find_element(By.XPATH, "//span[contains(text(),'People')]")
 people.click()
 
 time.sleep(2)
 name_idol = driver.find_element(By.XPATH, "//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div/div[3]/section/div/div/div[1]/div/div/button/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span/span[1]")
->>>>>>> 40f7eae8317acfc20488b2430bac7a698f4f2eec
 name_idol.click()
 
 #
@@ -68,18 +53,15 @@ resports=[]
 #view=[]
 #tweetIMG=[]
 
-<<<<<<< HEAD
 #Tim xem co bao nhieu post
 TWEETs = driver.find_elements(By.XPATH, "//article[@data-testid='tweet']")
 while True:
-=======
-userID = driver.find_element(By.XPATH, "//div[data-testid='User-Name']").text
-timePost = driver.find_element(By.XPATH,"//time").get_attribute("datetime")
-tweetText = driver.find_element(By.XPATH,"//div[data-testid='tweetText']")
-like = driver.find_element(By.XPATH,"//div[data-testid='like']")
-reply = driver.find_element(By.XPATH,"//div[data-testid='reply']")
-resport = driver.find_element(By.XPATH,"//div[data-testid='retweet']")
->>>>>>> 40f7eae8317acfc20488b2430bac7a698f4f2eec
+    userIDs = driver.find_element(By.XPATH, "//div[data-testid='User-Name']").text
+    timePost = driver.find_element(By.XPATH,"//time").get_attribute("datetime")
+    tweetText = driver.find_element(By.XPATH,"//div[data-testid='tweetText']")
+    like = driver.find_element(By.XPATH,"//div[data-testid='like']")
+    reply = driver.find_element(By.XPATH,"//div[data-testid='reply']")
+    resport = driver.find_element(By.XPATH,"//div[data-testid='retweet']")
 
     for TWEET in TWEETs:
 
